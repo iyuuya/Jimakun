aea3da11bbd28c90ea3cd86a3be81ad2f0182e8e
iyuuya <i.yuuya@gmail.com>
2016-01-12 07:55:04 +0900

    modify options of how to specify

--- main.m
+++ main.m
@@ -1,61 +1,101 @@
 #import "AppDelegate.h"
 #import "SubtitleWindowController.h"
 
-NSString *s2ns(const char *s)
-{
-  if (s) return [NSString stringWithCString:s encoding:NSUTF8StringEncoding];
-  else   return @"";
-}
-
 void printUsage(void)
 {
   printf(
-    "usage:\n"
-    "  jimakun \"messages ...\" [color_code] [edge_color_code] [thickness] [font_name font_size]\n"
-    "    colo_code:      RRGGBB\n"
-    "    edge_colo_code: RRGGBB\n"
-    "    thickness:      0-20\n"
-    "    font_name:      \"Font Name\"\n"
-    "    font_size:      1-N\n"
+    "Usage: jimakun [OPTIONS] [MESSAGES]\n"
+    "\n"
+    "Set Options:\n"
+    "  -c --color [RRGGBB]      set subtitle color\n"
+    "  -e --edge-color [RRGGBB] set subtitle edge color\n"
+    "  -t --thickness [N]       set thickness(N:0-20)\n"
+    "  -f --font [NAME SIZE]    set font name and size\n"
+    "\n"
+    "Helper Options:\n"
+    "  -h --help                show this message\n"
+    "     --font-names        -- show font names\n"
+    "\n"
   );
 }
 
 void printFontNames(void)
 {
   for (NSString *font_name in [[NSFontManager sharedFontManager] availableFonts]) {
-    printf("%s\n",
-        [font_name cStringUsingEncoding:NSUTF8StringEncoding]);
+    printf("%s\n", [font_name cStringUsingEncoding:NSUTF8StringEncoding]);
   }
 }
 
-int main(int argc, const char * argv[])
+void ArgumentError(void)
 {
-  if (argc <= 1 || argc == 6) {
-    printUsage();
-    return 1;
-  }
+  fprintf(stderr, "argument error!\n\n");
+  printUsage();
+  exit(0);
+}
 
-  if (argc == 2 && [s2ns(argv[1]) isEqual:@"-f"]) {
-    printFontNames();
-    return 0;
-  }
-
+int main(int argc, const char *argv[])
+{
   @autoreleasepool
   {
+    NSMutableArray *arguments = [[[NSProcessInfo processInfo] arguments] mutableCopy];
+
+    if (argc == 1 || [arguments containsObject:@"-h"] || [arguments containsObject:@"--help"]) {
+      printUsage();
+      return 0;
+    }
+
+    if ([arguments containsObject:@"--font-names"]) {
+      printFontNames();
+      return 0;
+    }
+
     NSApplication *app = [NSApplication sharedApplication];
     AppDelegate *delegate = [[AppDelegate alloc] init];
 
-    delegate.options = [@{@"subtitle": s2ns(argv[1])} mutableCopy];
+    delegate.options = [@{@"subtitle": [arguments lastObject]} mutableCopy];
+    [arguments removeLastObject];
+    argc--;
 
     if ([delegate.options[@"subtitle"] length] == 0)
       delegate.options[@"terminate"] = @"QUIT";
 
-    if (argc >= 3) delegate.options[@"color"] = s2ns(argv[2]);
-    if (argc >= 4) delegate.options[@"edge_color"] = s2ns(argv[3]);
-    if (argc >= 5) delegate.options[@"thickness"] = s2ns(argv[4]);
-    if (argc >= 7) {
-      delegate.options[@"font_name"] = s2ns(argv[5]);
-      delegate.options[@"font_size"] = s2ns(argv[6]);
+    NSUInteger idx = -1;
+
+    if (((idx = [arguments indexOfObject:@"-c"]) != NSNotFound) ||
+        ((idx = [arguments indexOfObject:@"--color"]) != NSNotFound)) {
+      if (argc > idx+1) {
+        delegate.options[@"color"] = arguments[idx+1];
+      } else {
+        ArgumentError();
+      }
+    }
+
+    if (((idx = [arguments indexOfObject:@"-e"]) != NSNotFound) ||
+        ((idx = [arguments indexOfObject:@"--edge-color"]) != NSNotFound)) {
+      if (argc > idx+1) {
+        delegate.options[@"edge_color"] = arguments[idx+1];
+      } else {
+        ArgumentError();
+      }
+    }
+
+    if (((idx = [arguments indexOfObject:@"-t"]) != NSNotFound) ||
+        ((idx = [arguments indexOfObject:@"--thickness"]) != NSNotFound)) {
+      if (argc > idx+1) {
+        delegate.options[@"thickness"] = arguments[idx+1];
+      } else {
+        ArgumentError();
+      }
+    }
+
+    if (((idx = [arguments indexOfObject:@"-f"]) != NSNotFound) ||
+        ((idx = [arguments indexOfObject:@"--font"]) != NSNotFound)) {
+      if (argc > idx+2) {
+        delegate.options[@"font_name"] = arguments[idx+1];
+        delegate.options[@"font_size"] = arguments[idx+2];
+      } else {
+        ArgumentError();
+      }
     }
 
     delegate.subtitle = [[SubtitleWindowController alloc] initWithText:delegate.options[@"subtitle"]];
